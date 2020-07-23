85b7b7b657207fcb7c63f2de932c4b4cb5396816
Chief-Queef <gebre903@gmail.com>
2020-07-22 21:04:52 -0400

    Update

---
+++ my_bot.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('Ready!')
+
+    client.user.setActivity("Homeless take a shower", {type: "WATCHING"})
+
+})
+client.on('message', message => {
+
+	if (message.content === 'nigga') {
+        message.channel.send('stfu homeless, stop saying the n-word, ugly ass');
+    }
+});
+//Token is NzM0MTQyNzUwNDY2OTAwMDY4.XxddYg.EvXHFFtOjU28QjK9Up9AAIGTMQg
+client.login(process.env.NzM0MTQyNzUwNDY2OTAwMDY4.XxddYg.EvXHFFtOjU28QjK9Up9AAIGTMQg);
